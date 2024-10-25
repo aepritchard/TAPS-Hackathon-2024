de206b42da8400a2bf40d066ab4a821386497d36
GoCatsWZ <weiqiang@ksu.edu>
2024-10-25 13:46:15 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -1,63 +1,28 @@
-
-
-
 library(shiny)
-library(bs4Dash)
-library(leaflet)
-library(mapdeck)
-library(highcharter)
-library(rsconnect)
-library(leaflet.extras2)
-library(DT)
-library(shinyWidgets)
 
-
-ui <- bs4DashPage(
-  
-  # ------------------------------- Header
-  header = bs4DashNavbar(
-    title = "StatFlow Dashboard",
-    compact = T,
-    fixed = T
-  ),
-  # ------------------------------- End Header
-  
-  # ------------------------------- Sidebar
-  sidebar = bs4DashSidebar(
-    
-  ),
-  # ------------------------------- End Sidebar
-  
-  # ------------------------------- Body
-  body = bs4DashBody(
-    
-  ),
-  # ------------------------------- End Body
-  
-  # ------------------------------- Control bar
-  controlbar = bs4DashControlbar(
-    
-  ),
-  # ------------------------------- End Control bar
-  
-  
-  # ------------------------------- Footer
-  footer = bs4DashFooter(
-    
-  )
-  # ------------------------------- End Footer
- 
+ui <- navbarPage(
+    "App Title",
+    tabPanel(
+        "Plot",
+        tags$style(
+        "body {  background-image: url('Folder2.png'); background-size: contain; background-repeat: no-repeat;  background-position: center;width: 100vw;
+    height: 100vh;} 
+        .navbar-nav li a {
+        font-size: 20px;
+        font-weight: bold;
+      }
+    "
+        ),
+        tabsetPanel(
+            tabPanel("A"),
+            tabPanel("B"),
+            tabPanel("C")
+        )
+    ),
+    tabPanel("Summary"),
+    tabPanel("Table")
 )
 
+server <- function(input, output) {}
 
-server <- function(input, output) {
-
-   
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
-
-
-
-  
+shinyApp(ui, server)
